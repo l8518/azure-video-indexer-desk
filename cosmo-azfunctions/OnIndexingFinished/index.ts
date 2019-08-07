b646383453cf05eeb3e0b0abd2ab35bbd5142d5f
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-08-07 21:46:42 +0200

    Add cosmosdb and function output

--- cosmo-azfunctions/OnIndexingFinished/index.ts
+++ cosmo-azfunctions/OnIndexingFinished/index.ts
@@ -2,11 +2,17 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // req.query.name || req.body.name
+    
+    // Log Input
     context.log(req.query);
     context.log(req.body);
+
+    // Set HTTP Output
     context.res = {};
+    // Output to Cosmos DB
+    context.bindings.exampleOutput = JSON.stringify({
+        hello: "world"
+      });
     context.done();
 };
 
