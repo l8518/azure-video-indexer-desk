d04b1d7f2c4095f4031e794b1748e063cd12a25f
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-08-11 13:38:52 +0200

    Improve layouting #1 - Add bootstrap v4 - Add layouting and partials

--- vi-desk-frontend/src/app.ts
+++ vi-desk-frontend/src/app.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,7 +12,9 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', '/views/_partials/'));
   app.setViewEngine('hbs');
+  app.set('view options', { layout: 'default' });
 
   await app.listen(process.env.PORT || 3000);
 }
